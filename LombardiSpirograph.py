3e593e50a8c435e6b9dabbab0c00f50a54e5c046
Rogério Brito <rbrito@ime.usp.br>
2012-12-31 00:03:00 -0200

    Apply semi-automated reformating of spaces around operators.

--- LombardiSpirograph.py
+++ LombardiSpirograph.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 """
 LombardiSpirograph - draw rotationally symmetric drawings in Lombardi style
 David Eppstein, UC Irvine, March 2010
@@ -8,7 +9,7 @@ For usage information type "python LombardiSpirography.py"
 without any additional arguments.
 """
 
-from pyx import canvas,path,color
+from pyx import canvas, path, color
 from optparse import OptionParser
 from math import *
 import sys
@@ -113,7 +114,7 @@ parser.add_option("-o","--outline", dest="outline", action="store_true",
 options,args = parser.parse_args()
 
 def abort(message):
-    print >>sys.stderr,message
+    print >> sys.stderr, message
     sys.exit(-1)
 
 graphName = "-".join(args).lower()
@@ -159,12 +160,12 @@ with respect to the inner two layers.
 if options.show_names:
     if graphName:
         if graphName not in namedGraphs:
-            print '''Graph name''',graphName,'''is not recognized.
+            print '''Graph name''', graphName, '''is not recognized.
 Run
     python LombardiSpirograph --names
 without any command line arguments to get a list of recognized names.'''
         else:
-            print graphName,"is equivalent to",namedGraphs[graphName]
+            print graphName, "is equivalent to", namedGraphs[graphName]
             sys.exit(0)
     print '''This program has built into it a set of graph names that may
 be used as the command-line argument to specify the graph to be
@@ -172,9 +173,9 @@ drawn. They are:
 '''
     graphs = namedGraphs.items()
     graphs.sort()
-    graphs = [("Name","Description"),("====","===========")] + graphs
-    for name,description in graphs:
-        print "    " + name + " "*(20-len(name)) + description
+    graphs = [("Name", "Description"), ("====", "===========")] + graphs
+    for name, description in graphs:
+        print "    " + name + " " * (20 - len(name)) + description
     sys.exit(0)
 
 if not graphName:
@@ -211,7 +212,7 @@ try:
     levels = len(vertexDescriptors)
     
     # Parse out the X for the descriptor at the inner level
-    central = [False]*levels
+    central = [False] * levels
     radialzero = False
     if vertexDescriptors[0] == "x":
         vertexDescriptors[0] = ""
@@ -222,7 +223,7 @@ try:
         radialzero = True
     
     # Parse out the letters for the circulant at each level
-    circulant = [None]*levels
+    circulant = [None] * levels
     for i in range(levels):
         circulant[i] = [ord(x) - ord('a') + 1 for x in vertexDescriptors[i]
                         if x >= "a" and x < "x"]
@@ -230,8 +231,8 @@ try:
 
     # Parse out the numbers for which other vertex at this level
     # connects to the same vertex at the next level
-    connector = [0]*levels
-    radial = [False]*levels
+    connector = [0] * levels
+    radial = [False] * levels
     for i in range(levels):
         if vertexDescriptors[i]:
             connector[i] = int(vertexDescriptors[i])
@@ -266,7 +267,7 @@ for offset in connector:
 #   Preliminary calculations
 # ============================================================
 
-stagger = [False]*levels
+stagger = [False] * levels
 s = False
 for i in range(levels):
     stagger[i] = s
@@ -275,35 +276,36 @@ for i in range(levels):
 if central[0] and radial[0]:
     stagger[0] = True
 
-inRadial = [False]*levels
+inRadial = [False] * levels
 for i in range(levels):
     if radial[i]:
-        conn = [j for j in range(i+1,levels) if stagger[i] == stagger[j]]
+        conn = [j for j in range(i + 1, levels) if stagger[i] == stagger[j]]
         if not conn:
             abort("No layer outward of %s with same stagger" % i)
         radial[i] = conn[0]
         inRadial[conn[0]] = True
 
-indegree = [1]*(levels+1)
+indegree = [1] * (levels + 1)
 for i in range(levels):
     if connector[i]:
-        indegree[i+1] += 1
+        indegree[i + 1] += 1
     if inRadial[i]:
         indegree[i] += 1
 indegree[0] = indegree[levels] = 0
 
 # Fudge factor for some angle computations
-incount = [indegree[i]+1 for i in range(levels)]
-if circulant[0] and circulant[0][0]*2 == symmetry:
+
+incount = [indegree[i] + 1 for i in range(levels)]
+if circulant[0] and circulant[0][0] * 2 == symmetry:
     incount[0] = 0
 
-degree = [0]*levels
+degree = [0] * levels
 for i in range(levels):
     if central[i]:
         degree[i] = symmetry
     else:
-        degree[i] = indegree[i] + indegree[i+1]
-        degree[i] += 2*len(circulant[i])
+        degree[i] = indegree[i] + indegree[i + 1]
+        degree[i] += 2 * len(circulant[i])
         if i == 0 and symmetry % 2 == 0 and symmetry // 2 in circulant[i]:
             degree[i] -= 1
 
@@ -313,24 +315,24 @@ for i in range(levels):
         if i == 0:
             if levels == 1:     # Only a single level
                 if len(circulant[i]) % 2 == 1:
-                    mid = len(circulant[i])//2
+                    mid = len(circulant[i]) // 2
             else:
                 if len(circulant[i]) % 2 == 0:
-                    mid = len(circulant[i])//2
+                    mid = len(circulant[i]) // 2
         elif i < levels - 1:
             if len(circulant[i]) % 2 == 1:
-                mid = len(circulant[i])//2
+                mid = len(circulant[i]) // 2
         elif len(circulant[i]) % 2 == 0:
-            mid = len(circulant[i])//2 - 1
+            mid = len(circulant[i]) // 2 - 1
     if mid >= 0 and mid < len(circulant[i]) and circulant[i][mid] != 1:
         abort("Central circulant edge can only be adjacent")
 
 # Test which edges within a single level follow line segments instead of arcs
-straightCirculant = [[False]*len(circulant[i]) for i in range(levels)]
+straightCirculant = [[False] * len(circulant[i]) for i in range(levels)]
 for i in range(levels):
     for j in range(len(circulant[i])):
-        if 2*(degree[i]*circulant[i][j]+symmetry*(2*j+incount[i])) \
-                == degree[i]*symmetry:
+        if 2 * (degree[i] * circulant[i][j] + symmetry *
+                (2 * j + incount[i])) == degree[i] * symmetry:
             straightCirculant[i][j] = True
 
 # ============================================================
@@ -341,31 +343,36 @@ def cot(x):
     """
     Cotangent(x). Why is this not in math?
     """
-    return tan(pi/2-x)
 
-def distance(p,q):
+    return tan(pi / 2 - x)
+
+
+def distance(p, q):
     """
     Euclidean distance from p to q.
     """
-    return ((p.real - q.real)**2 + (p.imag - q.imag)**2)**0.5
 
-radius = [0]*levels     # how far from origin to make each layer
-conrad = [0]*levels     # distance point to ctr of curvature of connector
-straightConnector = [False]*levels
+    return ((p.real - q.real) ** 2 + (p.imag - q.imag) ** 2) ** 0.5
+
+
+radius = [0] * levels  # how far from origin to make each layer
+conrad = [0] * levels  # distance point to ctr of curvature of connector
+straightConnector = [False] * levels
 for i in range(levels):
     if central[i]:
         radius[i] = 0
     elif i == 0:
-        radius[i] = symmetry / (2 * pi)     # arclen 1 around initial circle
-    elif connector[i-1] == 0:
-        factor = (1 + 2 * pi / symmetry)
-        if i > 1 and not circulant[i-1]:
-            if connector[i-2] % 2 != 0:
-                factor = (1 + factor)/2     # hack dodecahedron less exponential
-            elif radius[i-2]:
-                factor = min(factor,2-radius[i-2]/radius[i-1])
+        radius[i] = symmetry / (2 * pi)  # arclen 1 around initial circle
+    elif connector[i - 1] == 0:
+        factor = 1 + 2 * pi / symmetry
+        if i > 1 and not circulant[i - 1]:
+            if connector[i - 2] % 2 != 0:
+                factor = (1 + factor) / 2  # hack dodecahedron less exponential
+            elif radius[i - 2]:
+                factor = min(factor, 2 - radius[i - 2] / radius[i - 1])
+
                     # hack pappus and dyck to be less exponential
-        radius[i] = max(1,radius[i-1] * factor)
+        radius[i] = max(1, radius[i - 1] * factor)
         # same radial length as previous arclen
     else:
         # calculate some angles
@@ -375,23 +382,24 @@ for i in range(levels):
         #  O = point on line op on the other side of p
         #  c = center of curvature of connecting arc
         correction = 1
-        if radial[i-1]:
+        if radial[i - 1]:
             correction = 2
-        poq = pi*connector[i-1]/symmetry
-        pcq = (inRadial[i]+1)*pi/degree[i] - correction*pi/degree[i-1] + poq
-        Opq = correction*pi/degree[i-1] + pcq/2
+        poq = pi * connector[i - 1] / symmetry
+        pcq = (inRadial[i] + 1) * pi / degree[i] - correction * pi / degree[i - 1] + poq
+        Opq = correction * pi / degree[i - 1] + pcq / 2
         try:
-            radius[i] = radius[i-1] / ((cot(poq)-cot(Opq))*sin(poq))
+            radius[i] = radius[i - 1] / ((cot(poq) - cot(Opq)) * sin(poq))
         except:
-            abort("Unable to make connecting arcs from level %s to level %s" \
-                 % (i-1,i))
+            abort("Unable to make connecting arcs from level %s to level %s"
+                   % (i - 1, i))
         if abs(pcq) < 0.01:
-            straightConnector[i-1] = True
+            straightConnector[i - 1] = True
         else:
-            scr = distance(radius[i-1],radius[i]*e**(1j*poq))/2
-            conrad[i-1] = scr/sin(pcq/2)
-    if i > 0 and radius[i] < radius[i-1]:
+            scr = distance(radius[i - 1], radius[i] * e ** (1j * poq)) / 2
+            conrad[i - 1] = scr / sin(pcq / 2)
+    if i > 0 and radius[i] < radius[i - 1]:
         abort("Inverted levels")
+
 
 # ============================================================
 #   Generate graphical objects
@@ -400,8 +408,9 @@ for i in range(levels):
 def rotations(level):
     if central[level]:
         return [1j]
-    return [1j*e**(pi*1j*i/symmetry)
-            for i in range(int(stagger[level]),2*symmetry,2)]
+    return [1j * e ** (pi * 1j * i / symmetry) for i in
+            range(int(stagger[level]), 2 * symmetry, 2)]
+
 
 def vertices():
     """
@@ -409,37 +418,39 @@ def vertices():
     """
     for i in range(levels):
         for r in rotations(i):
-            yield radius[i]*r
+            yield radius[i] * r
+
 
 def segments():
     """
     Return sequence of vertex pairs to be connected by line segments.
     """
-    if circulant[0] and circulant[0][0]*2 == symmetry:
+    if circulant[0] and circulant[0][0] * 2 == symmetry:
+
         # cross-edges in center
         for i in range(circulant[0][0]):
-            r = radius[0]*e**(pi*2j*i/symmetry)
-            yield 1j*r,-1j*r
+            r = radius[0] * e ** (pi * 2j * i / symmetry)
+            yield (1j * r, -1j * r)
     for i in range(levels):
         if indegree[i] == 1:
             for r in rotations(i):
-                yield radius[i-1]*r,radius[i]*r
+                yield (radius[i - 1] * r, radius[i] * r)
         for j in range(len(circulant[i])):
             if straightCirculant[i][j]:
                 p = radius[i]
-                q = p * e**(pi*2j*circulant[i][j]/symmetry)
+                q = p * e ** (pi * 2j * circulant[i][j] / symmetry)
                 for r in rotations(i):
-                    yield p*r,q*r
+                    yield (p * r, q * r)
         if straightConnector[i]:
             p = radius[i]
-            q = radius[i+1]
-            poq = e**(1j*pi*connector[i]/symmetry)
+            q = radius[i + 1]
+            poq = e ** (1j * pi * connector[i] / symmetry)
             for r in rotations(i):
-                yield p*r,q*poq*r
-                yield p*r,q*r/poq
+                yield (p * r, q * poq * r)
+                yield (p * r, q * r / poq)
         if radial[i]:
             for r in rotations(radial[i]):
-                yield radius[i]*r,radius[radial[i]]*r
+                yield (radius[i] * r, radius[radial[i]] * r)
 
 maxRadius = radius[-1]
 
@@ -458,30 +469,30 @@ def circulants():
             if straightCirculant[i][j]:
                 continue
             step = circulant[i][j]
-            if step*2 == symmetry:
+            if step * 2 == symmetry:
                 continue
             q = radius[i] * rot[step]
-            d = distance(q,p)
-            theta = pi*step/symmetry # angle p-origin-circle center
-            psi = pi*(2*j+incount[i])/degree[i]   # angle o-p-cc
-            bulgy = theta + psi > pi/2
+            d = distance(q, p)
+            theta = pi * step / symmetry  # angle p-origin-circle center
+            psi = pi * (2 * j + incount[i]) / degree[i]  # angle o-p-cc
+            bulgy = theta + psi > pi / 2
             if bulgy:
-                psi -= pi/2
+                psi -= pi / 2
             else:
-                psi += pi/2
-            rad = abs(d/(2 * sin(pi - theta - psi)))
-            phi = pi/2 - theta - psi         # angle q-p-cc
+                psi += pi / 2
+            rad = abs(d / (2 * sin(pi - theta - psi)))
+            phi = pi / 2 - theta - psi  # angle q-p-cc
             if bulgy:
-                rtoc = radius[i]*cos(theta) - rad*sin(phi)
-                maxRadius = max(maxRadius,rtoc+rad)
+                rtoc = radius[i] * cos(theta) - rad * sin(phi)
+                maxRadius = max(maxRadius, rtoc + rad)
             for k in range(symmetry):
-                r = e**(pi*2j*k/symmetry)
+                r = e ** (pi * 2j * k / symmetry)
                 if not bulgy:
-                    yield (p*r,q*r,rad)
-                elif theta + psi < pi/2:
-                    yield (q*r,p*r,rad)
+                    yield (p * r, q * r, rad)
+                elif theta + psi < pi / 2:
+                    yield (q * r, p * r, rad)
                 else:
-                    yield (p*r,q*r,-rad)
+                    yield (p * r, q * r, -rad)
 
 def connectors():
     """
@@ -490,15 +501,15 @@ def connectors():
     for i in range(levels):
         if connector[i] and not straightConnector[i]:
             p = radius[i]
-            q = radius[i+1]
-            poq = e**(1j*pi*connector[i]/symmetry)
+            q = radius[i + 1]
+            poq = e ** (1j * pi * connector[i] / symmetry)
             for r in rotations(i):
                 if conrad[i] > 0:
-                    yield q*poq*r,p*r,conrad[i]
-                    yield p*r,q*r/poq,conrad[i]
+                    yield (q * poq * r, p * r, conrad[i])
+                    yield (p * r, q * r / poq, conrad[i])
                 else:
-                    yield p*r,q*poq*r,-conrad[i]
-                    yield q*r/poq,p*r,-conrad[i]
+                    yield (p * r, q * poq * r, -conrad[i])
+                    yield (q * r / poq, p * r, -conrad[i])
 
 def arcs():
     """
@@ -519,42 +530,42 @@ for a in arcs():
     pass
 scale = 25 * options.scale
 vertexRadius = 5 * options.radius * options.scale
-bbox = maxRadius*scale + vertexRadius + 1
-bbox = 1+int(bbox)
+bbox = maxRadius * scale + vertexRadius + 1
+bbox = 1 + int(bbox)
 
 print '''<?xml version="1.0" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" 
   "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 <svg width="%s" height="%s" viewBox="-%s -%s %s %s"
      xmlns="http://www.w3.org/2000/svg" version="1.1">''' % \
-  (2*bbox+1,2*bbox+1,0,0,2*bbox+1,2*bbox+1)
+    (2 * bbox + 1, 2 * bbox + 1, 0, 0, 2 * bbox + 1, 2 * bbox + 1)
 
 def place(p):
     """
     Convert unit-free complex to pixel-scaled real coordinates.
     """
-    return p.real*scale+bbox+1,p.imag*scale+bbox+1
+    return (p.real * scale + bbox + 1, p.imag * scale + bbox + 1)
 
 print '  <g style="fill:none; stroke:black">'
 
-for p,q in segments():
+for (p, q) in segments():
     print '    <line x1="%0.2f" y1="%0.2f" x2="%0.2f" y2="%0.2f" />' % \
-        (place(p)+place(q))
+        (place(p) + place(q))
 
-for p,q,r in arcs():
+for (p, q, r) in arcs():
     la = 0
     if r < 0:   # flag for large arc
         la = 1
         r = -r
     print '    <path d="M %0.2f,%0.2f A %0.2f,%0.2f 1 %s %s %0.2f,%0.2f" />' % \
-        (place(p)+(scale*r,scale*r,la,la)+place(q))
+        (place(p) + (scale * r, scale * r, la, la) + place(q))
 
 print '  </g>'
 print '  <g style="fill:%s; stroke:%s">' % (options.color, (options.outline and "none" or "black"))
 
 for v in vertices():
     print '    <circle cx="%0.2f" cy="%0.2f" r="%s" />' % \
-        (place(v)+(vertexRadius,))
+        (place(v) + (vertexRadius, ))
 
 print '  </g>'
 print '</svg>'
